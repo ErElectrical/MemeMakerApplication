4c19dc4a36554aafbd0ec1e3cfd2996d26be446d
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-03-04 16:26:31 +0000

    Added : Window forms for Application

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace MemeMakerApp
+{
+    partial class MemeMaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddtestBox = new System.Windows.Forms.GroupBox();
+            this.TopTextLabel = new System.Windows.Forms.Label();
+            this.txttopbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textbottombox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.imgpreview = new System.Windows.Forms.PictureBox();
+            this.openbutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.lbltoptext = new System.Windows.Forms.Label();
+            this.lblbottombox = new System.Windows.Forms.Label();
+            this.AddtestBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgpreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddtestBox
+            // 
+            this.AddtestBox.Controls.Add(this.button6);
+            this.AddtestBox.Controls.Add(this.button5);
+            this.AddtestBox.Controls.Add(this.button4);
+            this.AddtestBox.Controls.Add(this.button3);
+            this.AddtestBox.Controls.Add(this.button2);
+            this.AddtestBox.Controls.Add(this.button1);
+            this.AddtestBox.Controls.Add(this.label3);
+            this.AddtestBox.Controls.Add(this.textbottombox);
+            this.AddtestBox.Controls.Add(this.label2);
+            this.AddtestBox.Controls.Add(this.txttopbox);
+            this.AddtestBox.Controls.Add(this.TopTextLabel);
+            this.AddtestBox.Location = new System.Drawing.Point(22, 12);
+            this.AddtestBox.Name = "AddtestBox";
+            this.AddtestBox.Size = new System.Drawing.Size(297, 368);
+            this.AddtestBox.TabIndex = 0;
+            this.AddtestBox.TabStop = false;
+            this.AddtestBox.Text = "Add Tests";
+            this.AddtestBox.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // TopTextLabel
+            // 
+            this.TopTextLabel.AutoSize = true;
+            this.TopTextLabel.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TopTextLabel.Location = new System.Drawing.Point(32, 58);
+            this.TopTextLabel.Name = "TopTextLabel";
+            this.TopTextLabel.Size = new System.Drawing.Size(90, 22);
+            this.TopTextLabel.TabIndex = 0;
+            this.TopTextLabel.Text = "Top Text";
+            this.TopTextLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txttopbox
+            // 
+            this.txttopbox.Location = new System.Drawing.Point(35, 77);
+            this.txttopbox.Multiline = true;
+            this.txttopbox.Name = "txttopbox";
+            this.txttopbox.Size = new System.Drawing.Size(228, 44);
+            this.txttopbox.TabIndex = 1;
+            this.txttopbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Bottom Text";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textbottombox
+            // 
+            this.textbottombox.Location = new System.Drawing.Point(35, 164);
+            this.textbottombox.Multiline = true;
+            this.textbottombox.Name = "textbottombox";
+            this.textbottombox.Size = new System.Drawing.Size(238, 44);
+            this.textbottombox.TabIndex = 3;
+            this.textbottombox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(32, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 22);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Change Color";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(35, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 22);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(63, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 22);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gold;
+            this.button3.Location = new System.Drawing.Point(92, 277);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 22);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(35, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(22, 22);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Pink;
+            this.button5.Location = new System.Drawing.Point(63, 315);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(22, 22);
+            this.button5.TabIndex = 10;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LimeGreen;
+            this.button6.Location = new System.Drawing.Point(92, 315);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(22, 22);
+            this.button6.TabIndex = 11;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // imgpreview
+            // 
+            this.imgpreview.BackColor = System.Drawing.Color.Black;
+            this.imgpreview.Location = new System.Drawing.Point(325, 12);
+            this.imgpreview.Name = "imgpreview";
+            this.imgpreview.Size = new System.Drawing.Size(291, 368);
+            this.imgpreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgpreview.TabIndex = 1;
+            this.imgpreview.TabStop = false;
+            // 
+            // openbutton
+            // 
+            this.openbutton.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openbutton.Location = new System.Drawing.Point(114, 399);
+            this.openbutton.Name = "openbutton";
+            this.openbutton.Size = new System.Drawing.Size(79, 39);
+            this.openbutton.TabIndex = 2;
+            this.openbutton.Text = "open";
+            this.openbutton.UseVisualStyleBackColor = true;
+            // 
+            // savebutton
+            // 
+            this.savebutton.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savebutton.Location = new System.Drawing.Point(436, 399);
+            this.savebutton.Name = "savebutton";
+            this.savebutton.Size = new System.Drawing.Size(79, 39);
+            this.savebutton.TabIndex = 3;
+            this.savebutton.Text = "save";
+            this.savebutton.UseVisualStyleBackColor = true;
+            // 
+            // lbltoptext
+            // 
+            this.lbltoptext.BackColor = System.Drawing.Color.White;
+            this.lbltoptext.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltoptext.ForeColor = System.Drawing.Color.Black;
+            this.lbltoptext.Location = new System.Drawing.Point(325, 12);
+            this.lbltoptext.Name = "lbltoptext";
+            this.lbltoptext.Size = new System.Drawing.Size(291, 68);
+            this.lbltoptext.TabIndex = 4;
+            this.lbltoptext.Text = "Top text";
+            this.lbltoptext.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblbottombox
+            // 
+            this.lblbottombox.BackColor = System.Drawing.Color.White;
+            this.lblbottombox.Font = new System.Drawing.Font("Cascadia Mono SemiBold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblbottombox.ForeColor = System.Drawing.Color.Black;
+            this.lblbottombox.Location = new System.Drawing.Point(325, 327);
+            this.lblbottombox.Name = "lblbottombox";
+            this.lblbottombox.Size = new System.Drawing.Size(291, 53);
+            this.lblbottombox.TabIndex = 5;
+            this.lblbottombox.Text = "Bottom text";
+            this.lblbottombox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblbottombox.Click += new System.EventHandler(this.lblbottombox_Click);
+            // 
+            // MemeMaker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblbottombox);
+            this.Controls.Add(this.lbltoptext);
+            this.Controls.Add(this.savebutton);
+            this.Controls.Add(this.openbutton);
+            this.Controls.Add(this.imgpreview);
+            this.Controls.Add(this.AddtestBox);
+            this.Name = "MemeMaker";
+            this.Text = "MemeMaker Application";
+            this.AddtestBox.ResumeLayout(false);
+            this.AddtestBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgpreview)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox AddtestBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txttopbox;
+        private System.Windows.Forms.Label TopTextLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textbottombox;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imgpreview;
+        private System.Windows.Forms.Button openbutton;
+        private System.Windows.Forms.Button savebutton;
+        private System.Windows.Forms.Label lbltoptext;
+        private System.Windows.Forms.Label lblbottombox;
+    }
+}
+
